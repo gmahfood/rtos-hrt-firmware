83aa6ecd28b5cf77a4c583f573a7e8ee33dc54d1
George Mahfood <gmahfood@liberty.edu>
2026-04-09 22:21:12 -0400

    cleanup: remove old stimulus and scoring tasks (merged into game controller)

--- include/task_scoring.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <STM32FreeRTOS.h>
-
-/**
- * @brief   Computes reaction delta, manages flash high score, drives buzzer
- *          Reads completed ReactionEvent_t from xDisplayQueue
- *          Priority: 2
- */
-void vScoringTask(void *pvParameters);
--- include/task_stimulus.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <STM32FreeRTOS.h>
-
-/* 
- * @brief   Fires RGB LED stimulus after a random delay
- *          Posts stimulus timestamp to xReactionQueue
- *          Priority: 2
- */
-void vStimulusTask(void *pvParameters);
